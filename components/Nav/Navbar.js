b5b238de9a8f8bbc1611c77f76f452028c77aac4
JasonDollar <elrey33@gmail.com>
2022-03-17 18:25:44 +0100

    Navbar links, icons

--- components/Nav/Navbar.js
+++ components/Nav/Navbar.js
@@ -1,7 +1,28 @@
 import Image from "next/image"
+import Link from "next/link"
+import { useRouter } from "next/router"
+import { useState } from "react"
 import styles from './Navbar.module.scss'
 
 const Navbar = ({ username }) => {
+  const [showDropdown, setShowDropdown] = useState(false)
+  const router = useRouter()
+
+  const handleOnClickHome = (e) => {
+    e.preventDefault()
+    router.push("/")
+  }
+
+  const handleOnClickMyList = (e) => {
+    e.preventDefault()
+    router.push("/browse/my-list")
+  }
+
+  const handleShowDropdown = (e) => {
+    e.preventDefault();
+    setShowDropdown(!showDropdown);
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
@@ -16,23 +37,30 @@ const Navbar = ({ username }) => {
         </a>
     
         <ul className={styles.navItems}>
-          <li className={styles.navItem}>Home</li>
-          <li className={styles.navItem2}>My list</li>
+          <li className={styles.navItem} onClick={handleOnClickHome}>Home</li>
+          <li className={styles.navItem2} onClick={handleOnClickMyList}>My list</li>
         </ul>
 
         <nav className={styles.navContainer}>
           <div>
-            <button className={styles.usernameBtn}>
+            <button className={styles.usernameBtn}  onClick={handleShowDropdown}>
               <p className={styles.username}>{username}</p>
-              {/* TODO Expand more icon */}
+              <Image
+                src={"/static/expand_more.svg"}
+                alt="Expand dropdown"
+                width="24px"
+                height="24px"
+              />
             </button>
 
-            <div className={styles.navDropdown}>
+            { showDropdown && <div className={styles.navDropdown}>
               <div>
-                <a href="#" className={styles.linkName}>Sign out</a>
-                <div className={styles.lineWrapper}></div>
+                <Link href="/login">
+                  <a className={styles.linkName}>Sign out</a>
+                </Link>
+              <div className={styles.lineWrapper}></div>
               </div>
-            </div>
+            </div>}
           </div>
         </nav>
       </div>
