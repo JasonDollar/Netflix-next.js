9266304daab81e5b08f5a1cc468dcf32ca87556e
JasonDollar <elrey33@gmail.com>
2022-03-20 10:14:36 +0100

    Card error handling

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -1,7 +1,9 @@
+import { useState } from 'react'
 import Image from "next/image"
 import styles from './Card.module.scss'
 
-const Card = ({ imgUrl, size }) => {
+const Card = ({ imgUrl, size = "medium" }) => {
+  const [imgSrc, setImgSrc] = useState(imgUrl)
 
   const classMap = {
     large: styles.lgItem,
@@ -9,14 +11,20 @@ const Card = ({ imgUrl, size }) => {
     small: styles.smItem,
   }
 
+  const handleOnError = (error) => {
+    // defaulut image
+    setImgSrc('https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1159&q=80')
+  }
+
   return (
     <div className={styles.container}>
       <div className={classMap[size]}>
         <Image 
-          src={imgUrl}
+          src={imgSrc}
           alt="Movie image"
           layout="fill"
           className={styles.cardImg}
+          onError={handleOnError}
         />
 
       </div>
